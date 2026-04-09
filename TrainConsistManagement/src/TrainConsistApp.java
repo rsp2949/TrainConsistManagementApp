d366921b2e884cb7e9dd3574550f1e4990f4634e
rsp2949 <praveenrsp10@gmail.com>
2026-04-09 15:25:13 +0530

    UC7 Sort bogies by capacity using Comparator

--- TrainConsistManagement/src/TrainConsistApp.java
+++ TrainConsistManagement/src/TrainConsistApp.java
@@ -1,25 +1,44 @@
 import java.util.*;
 
 public class TrainConsistApp {
+
+    // Bogie class
+    static class Bogie {
+        String name;
+        int capacity;
+
+        Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("==================================");
-        System.out.println(" UC6 - Map Bogie to Capacity ");
+        System.out.println(" UC7 - Sort Bogies by Capacity ");
         System.out.println("==================================\n");
 
-        Map<String, Integer> capacityMap = new HashMap<>();
+        List<Bogie> bogies = new ArrayList<>();
 
-        capacityMap.put("Sleeper", 72);
-        capacityMap.put("AC Chair", 56);
-        capacityMap.put("First Class", 24);
-        capacityMap.put("Cargo", 120);
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("General", 90));
 
-        System.out.println("Bogie Capacity Details:");
-
-        for (Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        System.out.println("Before Sorting:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
         }
 
-        System.out.println("\nUC6 completed...");
+        // Sorting
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        System.out.println("\nAfter Sorting:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
+
+        System.out.println("\nUC7 completed...");
     }
 }
